a0763fc93f7a784f5ad0c70f5f89db2651b3e577
javasac <sachin5kansal@gmail.com>
2025-02-06 19:22:39 -0600

    Core Java

---
+++ src/com/sachin/CoreJava/GenericsJava.java
@@ -0,0 +1,22 @@
+package com.sachin.CoreJava;
+
+class GenericsJava<T, U>
+{
+    private T t1;
+    private U u1;
+
+    GenericsJava(T t1, U u1)
+    {
+        this.t1 = t1;
+        this.u1 = u1;
+        System.out.println("Value 1 = " + this.t1);
+        System.out.println("Value 2 = " + this.u1);
+    }
+
+    public static void main(String[] args)
+    {
+        GenericsJava<Integer, String> num = new GenericsJava<Integer, String>(10, "Hello World");
+        GenericsJava<Integer, Integer> num1 = new GenericsJava<Integer, Integer>(10, 455);
+        GenericsJava<Double, Double> num3 = new GenericsJava<Double, Double>(1878660d, 4576765d);
+    }
+}
---
+++ src/com/sachin/CoreJava/HashEquals.java
@@ -0,0 +1,59 @@
+package com.sachin.CoreJava;
+
+public class HashEquals
+{
+    int age;
+    String name;
+
+    public HashEquals(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (o==this)
+        {
+            return true;
+        }
+        if (!(o instanceof HashEquals))
+        {
+            return false;
+        }
+
+        HashEquals he = (HashEquals)o;
+
+        if ((this.age==he.age) && (this.name==he.name))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode()
+    {
+        System.out.println(this.hashCode());
+        return this.hashCode();
+    }
+
+    public static void main(String args[])
+    {
+        HashEquals s1 = new HashEquals("Sachin", 25);
+        HashEquals s2 = new HashEquals("Sachin", 25);
+
+        if (s1.equals(s2))
+        {
+            System.out.println("Objects are equal.");
+        }
+        else
+        {
+            System.out.println("Objects are not equal.");
+        }
+    }
+}
